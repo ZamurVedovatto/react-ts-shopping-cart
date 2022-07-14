a8a7b707f9eeb0efcbe5b88cb25d39a7be4eef3d
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-07-14 17:28:56 -0300

    form setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import { BrowserRouter } from "react-router-dom"
+
 import "./main.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter } from "react-router-dom"
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/pages/FeedbackForm.tsx
+++ src/pages/FeedbackForm.tsx
@@ -1,33 +1,110 @@
-import { SectionWrapper } from './../assets/styles/index'
+import { useState } from 'react';
+import RangeSlider from 'react-bootstrap-range-slider';
 import styled from 'styled-components'
-import { Container, Form, Button} from 'react-bootstrap'
+import { SectionWrapper } from './../assets/styles/index'
+import { Container, Card, Form, FloatingLabel, Button} from 'react-bootstrap'
+
 
 const FeedbackFormComp = styled.section`
 
 `
 
 export function FeedbackForm() {
+  const [ value, setValue ] = useState(10); 
+  const [ finalValue, setFinalValue ] = useState(null);
+
   return (
     <SectionWrapper>
       <FeedbackFormComp>
         <Container>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Range</Form.Label>
-              <Form.Range />
-            </Form.Group>
+          <Card>
+            <Card.Header>Zamur Borges Vedovatto - 17/07/2002 (14:30)</Card.Header>
+            <Card.Body className="p-4">
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>HTML</Form.Label>
+                  <RangeSlider
+                    value={value}
+                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                    onAfterChange={e => setFinalValue(e.target.value)}
+                    min={0}
+                    max={10}
+                    tooltipPlacement="top"
+                    tooltip="on"
+                    variant="secondary"
+                  />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>CSS/SCSS</Form.Label>
+                  <RangeSlider
+                    value={value}
+                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                    onAfterChange={e => setFinalValue(e.target.value)}
+                    min={0}
+                    max={10}
+                    tooltipPlacement="top"
+                    tooltip="on"
+                    variant="secondary"
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Accessibility Proficiency</Form.Label>
+                  <RangeSlider
+                    value={value}
+                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                    onAfterChange={e => setFinalValue(e.target.value)}
+                    min={0}
+                    max={10}
+                    tooltipPlacement="top"
+                    tooltip="on"
+                    variant="secondary"
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <FloatingLabel controlId="floatingTextarea2" label="Comments">
+                    <Form.Control
+                      as="textarea"
+                      placeholder="Leave a comment here"
+                      style={{ height: '100px' }}
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  {['radio'].map((type) => (
+                    <div key={`inline-${type}`} className="mb-3">
+                      <Form.Check
+                        inline
+                        label="1"
+                        name="group1"
+                        type={type}
+                        id={`inline-${type}-1`}
+                      />
+                      <Form.Check
+                        inline
+                        label="2"
+                        name="group1"
+                        type={type}
+                        id={`inline-${type}-2`}
+                      />
+                      <Form.Check
+                        inline
+                        disabled
+                        label="3 (disabled)"
+                        type={type}
+                        id={`inline-${type}-3`}
+                      />
+                    </div>
+                  ))}
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
         </Container>
       </FeedbackFormComp>
     </SectionWrapper>
