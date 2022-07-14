cf16ddda12b6c9e518abad65ee56b2aa6864d613
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-07-14 14:58:30 -0300

    finished

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Shopping Cart With React and TypeScript
 
 https://hire.lever.co/developer/documentation
+https://www.youtube.com/watch?v=lATafp15HWA
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,19 +1,18 @@
-import storeItems from "./../data/items.json"
-import { Row, Col } from "react-bootstrap"
-import { StoreItem } from "./../components/StoreItem"
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+import storeItems from "../data/items.json"
 
 export function Store() {
-
-    return (
-        <>
-            <div>Store</div>
-            <Row md={2} xs={1} ls={3} className="g-3">
-                {storeItems.map(item => (
-                    <Col key={item.id}>
-                        <StoreItem {...item} />
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
